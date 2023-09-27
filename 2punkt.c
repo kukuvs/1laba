bb266191267cb9b7301a5fcc3c7b98e15ff20883
kukuvs <danil.bakhmetev05@gmail.com>
2023-09-28 00:48:22 +0400

    2

--- 2punkt.c
+++ 2punkt.c
@@ -1,54 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
-    int a, b = 5, c;
-    double x, y = -0.5, z;
-    printf("a=");
-    scanf("%d", &a);
+    double a = 5;
+    double b, c = 5;
+    int d;
 
-    x = c = a;
-    printf("x = c = a : a=%d c=%d x=%f\n", a, c, x);
+    printf("Введите значение b: ");
+    scanf("%lf", &b);
+    printf("Введите значение d: ");
+    scanf("%d", &d);
 
-    a += b;
-    printf("a += b : a=%d\n", a);
+    a = a + b - 2;
+    c = c + 1;
+    d = c - a + d;
 
-    x *= b + a;
-    printf("x *= b+a : x=%lf\n", x);
+    printf("a=%lf, b=%lf, c=%lf, d=%d\n", a, b, c, d);
 
-    b += a--;
-    printf("b += a-- : a=%d b=%d\n", a, b);
+    a = a * c;
+    c = c - 1;
 
-    x -= ++c;
-    printf("x -= ++c : c=%d x=%lf\n", c, x);
+    printf("a=%lf, b=%lf, c=%lf, d=%d\n", a, b, c, d);
 
-    c = a / b;
-    printf("c = a/b : c=%4d\n", c);
+    a = a / 10;
+    c = c / 2;
+    b = b - 1;
+    d = d * (c + b + a);
 
-    c = a % b;
-    printf("c = a%%b : c=%d\n", c);
+    printf("a=%lf, b=%lf, c=%lf, d=%d\n", a, b, c, d);
 
-    y += (a + 1) / a++;
-    printf("y += (a+1)/a++ : a=%d y=%.3lf y=%.0lf\n", a, y, y);
-
-    b = 3 * (y -= 0.6) + 2 * b + 1;
-    printf("b = 3*(y-=.6)+2*b+1 : b=%d y=%.1lf\n", b, y);
-
-    z = a / 2;
-    printf("z = a/2 : z = a/2 : z=%lf\n", z);
-
-    z = (double)a / 2;
-    printf("z = (double)a/2 : z=%lf\n", z);
-
-    y = (x = 5.7) / 2;
-    printf("y = (x = 5.7)/2 : x=%lf y=%lf\n", x, y);
-
-    y = (int)x / 2;
-    printf("y = (int)x/2 : y=%f\n", y);
-
-    z = (b - 3) / 2 - x / 5 + (c /= 2) + 1 / 4 * z - y++ + ++b / 3.0;
-    printf("z = (b-3)/2 - x/5 +(c/=2) + 1/4*z - y++ + ++b/3. :\na=%d b=%d c=%d x=%lf y=%lf z=%lf\n", a, b, c, x, y, z);
-
-    system("pause");
     return 0;
 }
